1693ce6b7279942e0c9d6567126471bef00c6eb0
Dan Dumitru <dan.dumitru@gmail.com>
2019-05-12 10:27:36 +0300

    tema rezolvata abstract classes

---
+++ src/main/java/exercitiu/AShape.java
@@ -0,0 +1,11 @@
+package exercitiu;
+
+public abstract class AShape {
+
+    //returns shape's area
+    public abstract double getArea();
+
+    //returns shape's perimeter
+    public abstract double getPerimeter();
+
+}
---
+++ src/main/java/exercitiu/Circle.java
@@ -0,0 +1,27 @@
+package exercitiu;
+
+public class Circle extends AShape {
+
+    private static final double PI = 3.14;
+    //Math.PI
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public double getArea() {
+        return PI * radius * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+return 2 * PI * radius;
+    }
+}
